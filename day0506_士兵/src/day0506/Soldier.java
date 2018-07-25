286850f4696edf46857a06add7be041ad987c955
蚕尹 <1543839509@qq.com>
2018-07-25 10:24:37 +0800

    day0506_士兵

---
+++ day0506_士兵/src/day0506/Soldier.java
@@ -0,0 +1,45 @@
+package day0506;
+
+import java.util.Random;
+
+/**
+ * 封装：
+ * 		把士兵相关的属性数据，方法代码
+ * 		封装成一个独立的组件（类）
+ */
+public class Soldier {
+	//属性数据，成员变量
+	int id;	//默认为零
+	int blood = 100;
+	
+	//day06新加内容：构造方法
+	public Soldier(){
+		System.out.println("新建soldier对象是执行");
+	}
+	
+	
+	//成员方法
+	public void go(){
+		System.out.println(id + "号士兵前进");
+	}
+	
+	public void attact(){
+		System.out.println(id + "号士兵进攻");
+		//随机的减血量
+		int d =  new Random().nextInt(10);
+		blood -= d;
+		
+		//如果血量变成负数，置成0 
+		if(blood < 0){
+			blood = 0;			
+		}
+		
+		System.out.println("血量： " + blood );
+		
+		if(blood == 0){
+			System.out.println(id + "号士兵阵亡");
+			
+			return;
+		}
+	}
+}
